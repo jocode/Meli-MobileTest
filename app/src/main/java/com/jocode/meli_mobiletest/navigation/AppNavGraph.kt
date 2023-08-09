8f0c9ce98e50e2687635f7868c4704707a074f38
Johan Mosquera <joncamilomoni@gmail.com>
2023-08-09 10:46:07 -0500

    Pass argument to DetailScreen and setup navigation
    Signed-off-by: Johan Mosquera <joncamilomoni@gmail.com>

--- app/src/main/java/com/jocode/meli_mobiletest/navigation/AppNavGraph.kt
+++ app/src/main/java/com/jocode/meli_mobiletest/navigation/AppNavGraph.kt
@@ -1,9 +1,14 @@
 package com.jocode.meli_mobiletest.navigation
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
-import com.jocode.search.presentation.navigation.searchNavigation
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.jocode.navigation.Screens
+import com.jocode.search.navigation.searchNavigation
 
 @Composable
 fun AppNavGraph(
@@ -14,6 +19,20 @@ fun AppNavGraph(
         startDestination = startDestination,
         navController = navController
     ) {
-        searchNavigation()
+        searchNavigation(onNavigateToItemDetail = {
+            navController.navigate(Screens.Detail.passItemId(it))
+        })
+
+        composable(
+            route = Screens.Detail.route,
+            arguments = listOf(
+                navArgument(name = Screens.Detail.ITEM_ID_NAV_KEY) {
+                    type = NavType.StringType
+                    nullable = false
+                }
+            )
+        ) {
+            Text(text = "Detail Screen ${it.arguments?.getString(Screens.Detail.ITEM_ID_NAV_KEY)}")
+        }
     }
 }
--- feature/search/src/main/java/com/jocode/search/SearchViewModel.kt
+++ feature/search/src/main/java/com/jocode/search/SearchViewModel.kt
@@ -1,4 +1,4 @@
-package com.jocode.search.presentation
+package com.jocode.search
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
