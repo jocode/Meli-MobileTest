89ba1fe2de613fa2e6371541587910e3461352c7
Johan Mosquera <joncamilomoni@gmail.com>
2023-08-10 01:32:08 -0500

    Adding Pagination in Search, Define App Icon and SplashScreen
    Signed-off-by: Johan Mosquera <joncamilomoni@gmail.com>

--- app/src/main/java/com/jocode/meli_mobiletest/MainActivity.kt
+++ app/src/main/java/com/jocode/meli_mobiletest/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.jocode.meli_mobiletest.navigation.AppNavGraph
@@ -16,6 +17,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             MeliMobileTestTheme(dynamicColor = false) {
 
