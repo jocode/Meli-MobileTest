977a6a31273883fef9fe1ade1dd9ea8b2b04c0e2
Johan Mosquera <joncamilomoni@gmail.com>
2023-08-10 08:06:29 -0500

    Fixing error screen on Search feature

--- feature/search/src/main/java/com/jocode/search/SearchResultUiState.kt
+++ feature/search/src/main/java/com/jocode/search/SearchResultUiState.kt
@@ -1,11 +1,8 @@
 package com.jocode.search
 
-import com.jocode.utils.UiText
-
 sealed interface SearchResultUiState {
     object Loading : SearchResultUiState
     object EmptyQuery : SearchResultUiState
     object Loaded : SearchResultUiState
 
-    data class LoadFailed(val message: UiText) : SearchResultUiState
 }
